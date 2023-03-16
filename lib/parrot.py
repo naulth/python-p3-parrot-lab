bda09c1a049407a245b4d88f7fb1b26478ea1c9a
Hayden Nault <naulth@gmail.com>
2023-03-16 12:37:35 -0400

    passing tests

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,7 @@
-def parrot():
-    pass
+def parrot(string = None):
+    if (string != None):
+        print(f'{string}')
+        return string
+    else:
+        print("Squawk!")
+        return "Squawk!"
